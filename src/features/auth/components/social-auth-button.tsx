66356e419964d18e150b6f0346572fb6d102e527
Browny <codingbrowny@gmail.com>
2026-03-30 04:12:35 +0000

    Dev (#33)

--- src/features/auth/components/social-auth-button.tsx
+++ src/features/auth/components/social-auth-button.tsx
@@ -9,7 +9,7 @@ function SocialAuthButtons() {
     const {error} = await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: `${process.env.NEXT_PUBLIC_URL}/auth/callback`,
+        redirectTo: `${window.location.origin}/auth/callback`,
       },
     });
 
